4137577c89e46fd275c3e4c9e0b56306a855f993
Maaagggi <meyyappanmageswaran@gmail.com>
2024-03-04 15:38:36 +0530

    Updated code to setup customer class

--- create_account.py
+++ create_account.py
@@ -1,6 +1,6 @@
 # For generating Account number
-from account import Account
-from customer import Customer
+from .account import Account
+from .customer import Customer
 import uuid
 
 class CreateNewAccount:
